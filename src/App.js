f47deb31e74f1b109182060d0210fbf5ab57c518
Rishi-R-Shetty <rishiravi152005@gmail.com>
2024-05-03 14:33:03 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import AddExpenseForm from './components/AddExpenseForm';
+import Budget from './components/Budget';
+import ExpenseList from './components/ExpenseList';
+import ExpenseTotal from './components/ExpenseTotal';
+import RemainingBudget from './components/Remaining';
+import { AppProvider } from './context/AppContext';
+
+const App = () => {
+	return (
+		<AppProvider>
+			<div className='container'>
+				<h1 className='mt-3'>Budgetify.com</h1>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+						<Budget />
+					</div>
+					<div className='col-sm'>
+						<RemainingBudget />
+					</div>
+					<div className='col-sm'>
+						<ExpenseTotal />
+					</div>
+				</div>
+				<h3 className='mt-3'>Expenses</h3>
+				<div className='row '>
+					<div className='col-sm'>
+						<ExpenseList />
+					</div>
+				</div>
+				<h3 className='mt-3'>Add Expense</h3>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+						<AddExpenseForm />
+					</div>
+				</div>
+			</div>
+		</AppProvider>
+	);
+};
 
 export default App;
